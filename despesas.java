96ff668cba756d394d5d31955898e80c227f4ede
rafael <rafaelforti.contato@gmail.com>
2022-08-21 13:45:04 -0300

    scannerFix

--- despesas.java
+++ despesas.java
@@ -3,22 +3,21 @@ import java.util.Scanner;
 public class despesas {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Scanner rendaScanner = new Scanner(System.in);
         System.out.println("Qual a sua renda mensal?");
-        double renda = rendaScanner.nextDouble();
+        double renda = sc.nextDouble();
 
-        Scanner luzScanner = new Scanner(System.in);
         System.out.println("valor conta de luz?");
-        double luz = luzScanner.nextDouble();
+        double luz = sc.nextDouble();
 
-        Scanner aguaScanner = new Scanner(System.in);
+
         System.out.println("valor conta de agua?");
-        double agua = aguaScanner.nextDouble();
+        double agua = sc.nextDouble();
 
-        Scanner internetScanner = new Scanner(System.in);
+
         System.out.println("valor conta de internet?");
-        double internet = aguaScanner.nextDouble();
+        double internet = sc.nextDouble();
 
         double despesa = luz + agua + internet;
         System.out.println("voce gastou: " + despesa);
